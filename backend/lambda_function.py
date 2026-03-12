02c683ad9c6790bf871edf0f499ca4f7d59e5468
Constantin Suruniuc <costy.suruniu@proton.me>
2026-03-12 19:43:18 +0200

    add terraform config for aws setup

---
+++ backend/lambda_function.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("visitor-count")
+
+def lambda_handler(event, context):
+    response = table.update_item(
+        Key={"id": "counter"},
+        UpdateExpression="ADD visitor_count :inc",
+        ExpressionAttributeValues={":inc": 1},
+        ReturnValues="UPDATED_NEW"
+    )
+
+    count = int(response["Attributes"]["visitor_count"])
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps({"count": count})
+    }
---
+++ infra/main.tf
@@ -0,0 +1,184 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# S3 bucket for the static site
+resource "aws_s3_bucket" "resume" {
+  bucket = "constantin-suruniuc-cloud-resume"
+}
+
+resource "aws_s3_bucket_public_access_block" "resume" {
+  bucket = aws_s3_bucket.resume.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "resume" {
+  bucket = aws_s3_bucket.resume.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "resume" {
+  bucket = aws_s3_bucket.resume.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.resume.arn}/*"
+      }
+    ]
+  })
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "resume" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.resume.website_endpoint
+    origin_id   = "s3-resume"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-resume"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# DynamoDB table for the visitor counter
+resource "aws_dynamodb_table" "visitor_count" {
+  name         = "visitor-count"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+# IAM role for the Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "visitor-counter-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda function
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "../backend/lambda_function.py"
+  output_path = "../backend/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "visitor_counter" {
+  function_name    = "visitor-counter"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# API Gateway
+resource "aws_apigatewayv2_api" "visitor_counter" {
+  name          = "visitor-counter-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id             = aws_apigatewayv2_api.visitor_counter.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.visitor_counter.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "count" {
+  api_id    = aws_apigatewayv2_api.visitor_counter.id
+  route_key = "GET /count"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.visitor_counter.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.visitor_counter.execution_arn}/*/*"
+}
