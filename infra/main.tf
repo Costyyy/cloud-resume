abece27975fa37445045c5cd9b5296221e539b39
Constantin Suruniuc <costy.suruniu@proton.me>
2026-03-12 20:10:55 +0200

    add remote state for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "constantin-suruniuc-tf-state"
+    key    = "cloud-resume/terraform.tfstate"
+    region = "eu-north-1"
+  }
 }
 
 provider "aws" {
